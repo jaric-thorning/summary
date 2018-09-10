94fb89d1de40a6802043eef4dbcbc4a3f2cfb149
Jaric Thorning <jaric.thorning@gmail.com>
2018-09-10 16:42:25 +1000

    Added reimbursement sections to PDF, added unable to process files logging

--- summary.py
+++ summary.py
@@ -5,7 +5,7 @@ from stat import *
 
 from fpdf import FPDF
 
-
+ignoreFiles = [".DS_Store", "Icon\r"]
 printReceipts = False
 
 class Receipt:
@@ -19,17 +19,6 @@ class Receipt:
                 self.date = None
                 self.account = account
 
-        def setValuesFromName(self):
-                values = self.name.split('-')
-                if(len(values) != 5):
-                        self.name = "ERROR"
-                
-                else:
-                        self.paidBy = values[0]
-                        self.date = values[1]
-                        self.description = values[2]
-                        self.amount = float(values[3].strip()[1:])
-                        self.paidDate = values[4]
 
         def __str__(self):
             return "Account: %s - %s" % (self.account, self.name)
@@ -79,6 +68,11 @@ class Account:
         return self.getPrint()
 
     def __lt__(self, other):
+
+        if(self.name == "Grand Total"): 
+            return False
+        if(other.name == "Grand Total"): 
+            return True
         return self.name < other.name
 
 
@@ -94,6 +88,8 @@ def walktree(top, callback, account):
 
         subAccounts.append(x)
 
+        errorReceipts = []
+
         for f in os.listdir(top):
                 pathname = os.path.join(top, f)
                 mode = os.stat(pathname)[ST_MODE]
@@ -106,8 +102,11 @@ def walktree(top, callback, account):
                         if(account != "Grand Total"):
                             nextAccount = account + "/" + f
 
-                        totalAccount, returnedAccounts = walktree(pathname, callback, nextAccount)
-           
+                        totalAccount, returnedAccounts, errors = walktree(pathname, callback, nextAccount)
+        
+                        for e in errors:
+                            errorReceipts.append(e)
+
                         for a in returnedAccounts:
                             subAccounts.append(a) 
 
@@ -122,6 +121,10 @@ def walktree(top, callback, account):
                         newReceipt = processReceipt(f, x)
                         if(newReceipt != -1):
                             x.addReceipt(newReceipt)
+                        else: 
+                            if(f not in ignoreFiles):
+                                print(F"Error: {f}")
+                                errorReceipts.append(f)
                 else:
                         # Unknown file type, print a message    
                         print(F"Skipping {pathname}")
@@ -129,7 +132,7 @@ def walktree(top, callback, account):
 
         subAccounts.sort()
         #x.printAccount()
-        return x.getSum(), subAccounts
+        return x.getSum(), subAccounts, errorReceipts
 
 
 def processReceipt(receiptString, account):
@@ -147,8 +150,8 @@ def processReceipt(receiptString, account):
     elif(partsLen == 5):
         #potential paid receipt
        
-
-        newReceipt.paidDate = parts[4][parts[4].find("("):parts[4].find(")")]
+        if "PAID" in parts[4] and "UNPAID" not in parts[4]:
+            newReceipt.paidDate = parts[4][parts[4].find("("):parts[4].find(")")]
         #attempt to extract paid date 
     else: 
         return -1
@@ -200,16 +203,67 @@ def walktreeiterative(top):
         dirStack.remove(parent)
 
 
-def generatePDF(accounts):
+def paidTo(accounts, filter): 
+
+    paid = {}
+
+    for a in accounts:
+        for r in a.receipts:
+            if r.paidBy != None:
+                if(filter == "PAID"):
+                    if(r.paidDate == None):
+                        break
+                elif(filter == "UNPAID"):
+                    if(r.paidDate != None):
+                        break
+
+                if paid.get(r.paidBy) == None:
+                    paid[r.paidBy] = r.amount
+                else:
+                    paid[r.paidBy] += r.amount
+
+    return paid
+
+def generatePDF(directory):
+
     print("Generating PDF...")
+
+    totalamount, accounts, errorReceipts = walktree(directory, visitfile, "Grand Total")
+
+    foundRoot = False
+    relativeDir = ""
+    localDir = ""
+    directoryLookBack = -1
+    while(localDir == ""):
+        localDir = directory.split("/")[directoryLookBack]
+        directoryLookBack -= 1
+        if(directoryLookBack > len(directory.split("/"))):
+            localDir = directory
+            break
+    for d in directory.split("/"):
+        if(d == "1.0 Financial"):
+            foundRoot = True
+
+        if foundRoot:
+            relativeDir += d
+            if(d != ""):
+                relativeDir += "/"
+
+    if(foundRoot == False):
+        relativeDir = directory
+
     pdf = FPDF(orientation = 'P', unit = 'mm', format='A4')
     pdf.add_page()
     pdf.set_font('Courier', 'B', 16)
     pdf.ln(15)
     now = datetime.date.today()
     pdf.cell(130, 10, F"UQ Sailing Club Accounts    {now}",1, 1, 'C')
-    pdf.ln(25)
+    pdf.ln(0)
 
+    pdf.set_font('Courier', 'B', 9)
+    pdf.cell(130, 10, F"/{relativeDir}",0, 1)
+
+    pdf.ln(10)
     pdf.set_font('Courier', 'B', 14)
     pdf.cell(130, 10, F"Receipts",0, 1)
     pdf.set_font('Courier', '', 11)
@@ -217,9 +271,68 @@ def generatePDF(accounts):
     pdf.image("uqsail.png", 150, 10, 40, 40, "", "")
     for a in accounts:
         #text = a.printFormat()
+        if(a.accountName == "Grand Total"):
+            pdf.set_font('Courier', 'B', 11)
         pdf.cell(0,7, F"{a.level * 3 * ' '}{a.accountName}".ljust(50) + "$" + F"{float(a.getSum()):.2f}".rjust(12), 0,1, 'C')
+        if(a.accountName == "Grand Total"):
+            pdf.set_font('Courier', '', 11)
+    
 
-    pdf.output('report.pdf', 'F')
+    pdf.add_page()
+    pdf.set_font('Courier', 'B', 14)
+    pdf.cell(130, 10, F"Reimbursements - Paid",0, 1)
+    pdf.set_font('Courier', '', 11)
+
+    paid = paidTo(accounts, "PAID")
+
+    print("\nPaid: ")
+    if(len(paid) != 0):
+
+        for person, amount in sorted(list(paid.items()), key=lambda x: x[1], reverse=True):
+            text = F"{person}".ljust(50) +"$"+ F"{float(amount):.2f}".rjust(12)
+            pdf.cell(0, 7, text,0, 1, 'C')
+            print(text)    
+    else: 
+        text = "None"
+        pdf.cell(0, 7, text,0, 1, 'C')
+        print(text) 
+    
+    pdf.set_font('Courier', 'B', 14)
+    pdf.cell(130, 10, F"Reimbursements - Yet to be paid",0, 1)
+    pdf.set_font('Courier', '', 11)
+
+    unpaid = paidTo(accounts, "UNPAID")
+
+    print("\nUnpaid: ")
+    if(len(unpaid) != 0):
+        for person, amount in sorted(list(unpaid.items()), key=lambda x: x[1], reverse=True):
+            text = F"{person}".ljust(50) +"$"+ F"{float(amount):.2f}".rjust(12)
+            pdf.cell(0, 7, text,0, 1, 'C')
+            print(text)
+    else: 
+        text = "None"
+        pdf.cell(0, 7, text,0, 1, 'C')
+        print(text) 
+
+    
+    pdf.set_font('Courier', 'B', 14)
+    pdf.cell(130, 10, F"Unable to Process",0, 1)
+    pdf.set_font('Courier', '', 11)
+
+    print("\nUnable to process")
+    if(len(errorReceipts) > 0):
+        
+        for e in errorReceipts:
+            text = e
+            pdf.cell(0, 7, text,0, 1, 'C')
+            print(text)
+    else: 
+        text = "None"
+        pdf.cell(0, 7, text,0, 1, 'C')
+        print(text) 
+
+    pdf.output(F'UQSail Receipts ({localDir}) - {datetime.date.today()}.pdf', 'F')
+    #pdf.output(F"report-{now}.pdf", 'F')
 
 def main():
     
@@ -249,7 +362,7 @@ def main():
     receipts = []
 
 
-    totalamount, returnedAccounts = walktree(directory, visitfile, "Grand Total")
+    totalamount, returnedAccounts, errorReceipts = walktree(directory, visitfile, "Grand Total")
     
     print(F"Total Amount is {totalamount}")
     for a in returnedAccounts:
@@ -257,7 +370,7 @@ def main():
 
     #walktreeiterative(directory)
 
-    generatePDF(returnedAccounts)
+    generatePDF(directory)
                 
 
 if __name__ == '__main__':
